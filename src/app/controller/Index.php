67d0213fbb77cdbedce773ee60b208a41a7333bd
962567040@qq.com <962567040@qq.com>
2020-03-02 18:03:29 +0800

    初始化

---
+++ src/app/controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+namespace lvtu\appdemo\controller;
+
+use app\BaseController;
+
+class Index extends BaseController
+{
+    public function index()
+    {
+        return 'appdemo工作流模块';
+    }
+
+}
---
+++ src/app/model/WorkFlow.php
@@ -0,0 +1,14 @@
+<?php
+declare (strict_types = 1);
+
+namespace lvtu\appdemo\model;
+
+/**
+ * @mixin think\Model
+ */
+class appdemo extends app\model\Base
+{
+
+
+    
+}
