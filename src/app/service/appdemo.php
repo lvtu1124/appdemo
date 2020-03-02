5be6ef53939c1b84347c9daf55862a9b6642c1df
962567040@qq.com <962567040@qq.com>
2020-03-02 21:53:35 +0800

    aaaaaaaaa

--- src/app/service/appdemo.php
+++ src/app/service/appdemo.php
@@ -1,13 +1,10 @@
 <?php
-
-
 namespace lvtu\appdemo\service;
-
 
 class Appdemo extends \think\Service
 {
     public function boot()
     {
-        $this->loadRoutesFrom(app()->getBasePath().'vendor'.DIRECTORY_SEPARATOR.'lvtu1124'.DIRECTORY_SEPARATOR.'appdemo'.DIRECTORY_SEPARATOR.'route'.DIRECTORY_SEPARATOR.'Route.php');
+        $this->loadRoutesFrom(app()->getBasePath().'vendor'.DIRECTORY_SEPARATOR.'lvtu1124'.DIRECTORY_SEPARATOR.'appdemo'.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'route'.DIRECTORY_SEPARATOR.'Route.php');
     }
 }
