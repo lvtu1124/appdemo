d46599f119e223a1a1f6ea1911f502cbaf06f719
962567040@qq.com <962567040@qq.com>
2020-03-02 23:20:14 +0800

    ccccccccc

--- src/route/Route.php
+++ src/route/Route.php
@@ -10,7 +10,7 @@
 // +----------------------------------------------------------------------
 use think\facade\Route;
 
+Route::get('appdemo/index/:name', 'lvtu\appdemo\controller\index@index');
 Route::get('appdemo', function () {
     return 'hello,appdemo11111111!';
 });
-Route::get('appdemo/index/:name', 'lvtu\appdemo\controller\index@index');
